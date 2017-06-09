23aee7d2a75a03dd4b469e1a887eb5621a845a45
MattValenti <Matthew@Matthews-MBP.gateway>
2017-06-09 22:59:18 +1000

    reader-test1

---
+++ assets/js/main.js
@@ -0,0 +1,129 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+
+
+$(document).ready(function() {
+  // Add jQuery here
+
+  $(".oase").mouseenter(function() {
+    $(".oase__image").addClass("showme");
+  });
+
+  $(".oase").mouseleave(function() {
+    $(".oase__image").removeClass("showme");
+  });
+
+  $(".magazine").mouseenter(function() {
+    $(".magazine__image").addClass("showme");
+  });
+
+  $(".magazine").mouseleave(function() {
+    $(".magazine__image").removeClass("showme");
+  });
+
+  $(".doggos").mouseenter(function() {
+    $(".doggos__image").addClass("showme");
+  });
+
+  $(".doggos").mouseleave(function() {
+    $(".doggos__image").removeClass("showme");
+  });
+
+  $(".doggo").mouseenter(function() {
+    $(".doggo__image").addClass("showme");
+  });
+
+  $(".doggo").mouseleave(function() {
+    $(".doggo__image").removeClass("showme");
+  });
+
+  $(".google").mouseenter(function() {
+    $(".google__image").addClass("showme");
+  });
+
+  $(".google").mouseleave(function() {
+    $(".google__image").removeClass("showme");
+  });
+
+  $(".hipsters").mouseenter(function() {
+    $(".hipsters__image").addClass("showme");
+  });
+
+  $(".hipsters").mouseleave(function() {
+    $(".hipsters__image").removeClass("showme");
+  });
+
+  $(".cheese").mouseenter(function() {
+    $(".cheese__image").addClass("showme");
+  });
+
+  $(".cheese").mouseleave(function() {
+    $(".cheese__image").removeClass("showme");
+  });
+
+  $(".catpeeking").mouseenter(function() {
+    $(".catpeeking__image").addClass("showme");
+  });
+
+  $(".catpeeking").mouseleave(function() {
+    $(".catpeeking__image").removeClass("showme");
+  });
+
+  $(".simply").mouseenter(function() {
+    $(".simply__image").addClass("showme");
+  });
+
+  $(".simply").mouseleave(function() {
+    $(".simply__image").removeClass("showme");
+  });
+
+  $(".sick").mouseenter(function() {
+    $(".sick__image").addClass("showme");
+  });
+
+  $(".sick").mouseleave(function() {
+    $(".sick__image").removeClass("showme");
+  });
+
+  $(".weed").mouseenter(function() {
+    $(".weed__image").addClass("showme");
+  });
+
+  $(".weed").mouseleave(function() {
+    $(".weed__image").removeClass("showme");
+  });
+
+  $(".baby").mouseenter(function() {
+    $(".baby__image").addClass("showme");
+  });
+
+  $(".baby").mouseleave(function() {
+    $(".baby__image").removeClass("showme");
+  });
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
